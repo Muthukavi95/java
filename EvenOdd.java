74a235b1d9714bedfbe1e1805b0a3432f45da405
Muthukavi95 <43901581+Muthukavi95@users.noreply.github.com>
2018-10-06 14:58:16 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println( "  even");
+        else if (num > 0)
+            System.out.println( "  odd");
+            else
+             System.out.println( " invalid");
+
+    }
+}
